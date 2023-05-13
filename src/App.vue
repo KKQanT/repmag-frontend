a9f3e4b117f26ee4beb776a05aabf6f02b4d2807
unknown <asskarnwin@gmail.com>
2023-05-13 12:28:38 +0700

    add logout in nav

--- src/App.vue
+++ src/App.vue
@@ -214,7 +214,7 @@ export default {
   <InputProfilePage @emittedPageStatus="(value) => pageStatus = value" v-else-if='pageStatus === "inputProfile"' />
   <div v-else>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">Logo</a>
+      <a class="navbar-brand mx-2" href="#">Logo</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -229,6 +229,9 @@ export default {
           </li>
         </ul>
       </div>
+      <button class="btn btn-outline-danger mx-2" @click="logout"> 
+        logout
+      </button>
     </nav>
     <div>
       <Swipping v-if="pageStatus === 'swipping'" :recommended-users-props="recommendsUsers" />
