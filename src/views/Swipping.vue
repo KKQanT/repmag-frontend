8caa9ff7547ff87788cce5be58e9d76afa54c932
unknown <asskarnwin@gmail.com>
2023-05-20 10:34:23 +0700

    change close button modal

--- src/views/Swipping.vue
+++ src/views/Swipping.vue
@@ -84,8 +84,7 @@ export default {
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">{{ selectedPartner?.name }}</h5>
-                    <button type="button" class="close" @click="closeCard">
-                        <span>&times;</span>
+                    <button type="button" class="btn-close" @click="closeCard">
                     </button>
                 </div>
                 <div class="modal-body">
