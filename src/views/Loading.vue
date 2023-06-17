8ff154c7dd9b0ab09460178674260bae99fc7c63
unknown <asskarnwin@gmail.com>
2023-06-17 22:09:33 +0700

    fix loading icon

--- src/views/Loading.vue
+++ src/views/Loading.vue
@@ -1,72 +1,75 @@
 <script lang="ts">
 export default {
-    name: "Loading",
+  name: "Loading",
 }
 </script>
 
 <template>
-    <div class="loading-container">
-      <div class="spinner-border text-primary" role="status"></div>
-      <div class="heart-icon">
-        <svg width="10em" height="10em" viewBox="0 0 16 16"  xmlns="http://www.w3.org/2000/svg">
-          <path d="M8 14.7l-.7-.7C3.4 10.8.6 8.4.6 5.5c0-2.4 2-4.4 4.4-4.4 1.5 0 2.9.7 3.6 1.9h.8c.7-1.2 2.1-1.9 3.6-1.9 2.4 0 4.4 2 4.4 4.4 0 2.9-2.8 5.3-6.7 8.5L8 14.7z" />
-        </svg>
-      </div>
+  <div class="loading-container">
+    <div class="spinner-border text-primary" role="status"></div>
+    <div class="heart-icon">
+      <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" class="bi bi-heart-fill"
+        viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
+      </svg>
     </div>
-  </template>
+  </div>
+</template>
   
-  <style scoped>
-  .loading-container {
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
+<style scoped>
+.loading-container {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.spinner-border {
+  width: 10rem;
+  height: 10rem;
+  border-width: 0.2rem;
+  position: absolute;
+  animation: spin 2s linear infinite;
+  border-top-color: black;
+  border-left-color: black;
+  border-bottom-color: black;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
   }
-  
-  .spinner-border {
-    width: 10rem;
-    height: 10rem;
-    border-width: 0.2rem;
-    position: absolute;
-    animation: spin 2s linear infinite;
-    border-top-color: black;
-    border-left-color: black;
-    border-bottom-color: black;
+
+  100% {
+    transform: rotate(360deg);
   }
-  
-  @keyframes spin {
-    0% {
-      transform: rotate(0deg);
-    }
-    100% {
-      transform: rotate(360deg);
-    }
+}
+
+.heart-icon {
+  position: relative;
+  width: 5rem;
+  height: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: heartbeat 1s ease-in-out infinite;
+}
+
+.heart-icon svg {
+  fill: "#007bff";
+}
+
+@keyframes heartbeat {
+  0% {
+    transform: scale(1);
   }
-  
-  .heart-icon {
-    position: relative;
-    width: 5rem;
-    height: 5rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    animation: heartbeat 1s ease-in-out infinite;
+
+  50% {
+    transform: scale(1.2);
   }
-  
-  .heart-icon svg {
-    fill: "#007bff";
+
+  100% {
+    transform: scale(1);
   }
-  
-  @keyframes heartbeat {
-    0% {
-      transform: scale(1);
-    }
-    50% {
-      transform: scale(1.2);
-    }
-    100% {
-      transform: scale(1);
-    }
-  }
-  </style>
+}</style>
